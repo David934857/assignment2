18ed49a6932f4b5c78ae5d0c6182ce5448b7e1af
David934857 <vhapalwrighd@MPD-WS132344.v21.med.va.gov>
2015-06-19 12:11:50 -0700

    maxtrixassigment
    ma=t\asdfasdfasdf

---
+++ assignment_2.R
@@ -0,0 +1,46 @@
+
+# david Wright
+# R programming: Week 3: Lexical Scoping: Assignment 2
+
+
+ makeCacheMatrix <- function(x = matrix()) {
+
+        m <- NULL 					
+
+        set <- function(y) {   					
+                x <<- y
+                m <<- NULL 	
+        }
+        
+        get <- function() x 			
+        
+        setinverse <- function(inv) m <<- inv 	
+
+        getinverse <- function() m 		
+		
+		list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+ }
+
+
+
+ cacheSolve <- function(x, ...) {
+						
+        m <- x$getinverse()                   	
+
+        if(!is.null(m)){                       	
+
+                message("getting cached data")  
+
+                return(m)                      	
+        }
+
+        data <- x$get()                     	
+
+        m <- solve(data, ...)               	
+
+        x$setinverse(m)                     	
+ }
+
+
+        
+        
